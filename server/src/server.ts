196a8d319d28c7b2767d5c6333818aa0c3412b1c
Maboloc,John Carlo <johncarlo.maboloc@college.cksc.edu.ph>
2026-01-19 11:48:00 +0800

    initial files

---
+++ server/src/server.ts
@@ -0,0 +1,45 @@
+import express, { type Application, type Request, type Response } from 'express';
+
+const app: Application = express();
+const port = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World with TypeScript and Express!');
+});
+
+
+type UserType = {
+    id: number,
+    firstName: string,
+    lastName: string,
+}
+
+const users: UserType[] = [
+  { "id": 1, "firstName": "Caloy", "lastName": "Maboloc" },
+  { "id": 2, "firstName": "Jb", "lastName": " Canotr" },
+  { "id": 3, "firstName": "Jeromy", "lastName": "Soong" }
+];
+
+function shuffleArray(array: UserType[]) {
+  let currentIndex = array.length
+  while (0 !== currentIndex) {
+    const randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    const temporaryValue = array[currentIndex];
+    if (temporaryValue && array[randomIndex]) {
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+        }
+    }
+    return array;
+  }
+
+app.get('/api/users', (req, res) => {
+    shuffleArray(users);
+  res.status(200).json(users);
+});
+
+app.listen(port, () => {
+   console.log(`Server is running on http://localhost:${port}`);
+});
+
